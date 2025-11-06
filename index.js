d3826ab6f8117c5554e7134c50131ec1825b3633
keremgarip <kerem.garip@gmail.com>
2025-11-06 13:17:23 +0300

    cemberinAlani

--- index.js
+++ index.js
@@ -64,8 +64,8 @@ function CemberinCevresi(/* kodlar buraya */) {
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function CemberinAlani(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinAlani(radius, piSayi) {
+  return piSayi * Math.pow(radius, 2);
 }
 
 /* (Oto test yok) Yukarıdaki CemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
